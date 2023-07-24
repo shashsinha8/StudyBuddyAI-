6bf8ea8306efdf2f0f16745e3c15a19085113c45
shashsinha8 <shashsinha.us@gmail.com>
2023-07-24 04:58:25 -0500

    .env removed

--- README.md
+++
@@ -1,2 +0,0 @@
-# TutorAI
-Upload your pdf documents and make an AI talk to you about it
--- app.py
+++
@@ -1,7 +0,0 @@
-from webapp import create_app, request, jsonify
-from flask_cors import CORS
-
-# github token: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
-app = create_app()
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
